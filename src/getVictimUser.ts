16bcc0122b2b5e66ae824896d23f7c3350ecd6f7
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-05-01 12:38:15 -0700

    added csv demo

--- src/getVictimUser.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-export default async function getVictimUser() {
-  const victimUserId = process.env.VICTIM_USER_ID;
-  if (!victimUserId) {
-    throw new Error("VICTIM_USER_ID is not defined");
-  }
-  return victimUserId;
-}
